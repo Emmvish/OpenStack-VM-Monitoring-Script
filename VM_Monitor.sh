06a2a4c96fdaed4c4364423d91a6e6027039077c
Emmvish <76655197+Emmvish@users.noreply.github.com>
2021-04-20 12:22:58 +0530

    Update VM_Monitor.sh

--- VM_Monitor.sh
+++ VM_Monitor.sh
@@ -2,7 +2,7 @@ Diagnostics.sh
 
 #! /bin/bash
 
-source /root/apoorv-openrc
+source /root/user-openrc
 openstack project list > projects1a.txt
 sed -i 's/|//1' projects1a.txt
 sed -i 's/|//2' projects1a.txt
@@ -13,14 +13,11 @@ sed -i 's/+//g' projects1a.txt
 sed -i '/^$/d' projects1a.txt
 TENANT_ID=($(python parse1d.py| tr -d '[],'))
 TENANT_NAME=($(python parse2d.py| tr -d '[],'))
-#echo ${TENANT_ID[@]}
-#echo ${TENANT_NAME[@]}
 TENANTS=${#TENANT_ID[@]}
 sed -i "1s/#export/export/1" /root/apoorv-openrc
 for (( i=0; i<TENANTS; i++ ))
 do
 tname=$( echo ${TENANT_ID[i]} | sed "s/'//g" )
-#tname=$( echo ${TENANT_NAME[i]}| sed "/'//g" )
 sed -i "s/<project>/$tname/g" /root/apoorv-openrc
 source /root/apoorv-openrc
 openstack server list > projects1b.txt
@@ -46,13 +43,10 @@ openstack server list > projects1b.txt
 id=$( echo ${SERVER_ID[j]} | sed "s/'//g" )
 #echo $id
 sed -i "s/server-id/$id/g" command1.sh
-#cat command1.sh
 sed -i "s/server_diagnostics.json/server_diagnostics_$i.json/g" command1.sh
 x=${STATUSES[j]}
 STATUSES[j]=$(echo $x | sed "s/,//g")
-#echo ${STATUSES[j]}
 if [[ "${STATUSES[j]}" != 0 ]]; then
-#echo "Why me?"
 ./command1.sh
 sed -i "s/$id/server-id/g" command1.sh
 sed -i "s/[[:space:]]//g" "server_diagnostics_$i.json"
@@ -64,7 +58,6 @@ sed -i "s/,/,\n/g" test1.json
 python parse5d.py
 l=$( echo ${SERVER_NAME[j]} | sed "s/'//g" )
 m="${l,,}"
-#echo $m
 b=$( echo ${TENANT_NAME[i]} | sed "s/'//g" )
 a="${b,,}"
 #echo $a
@@ -87,96 +80,12 @@ done
 else
 echo "."
 fi
-sed -i "s/$tname/<project>/g" /root/apoorv-openrc
+sed -i "s/$tname/<project>/g" /root/user-openrc
 done
-sed -i "1s/export/#export/1" /root/apoorv-openrc
+sed -i "1s/export/#export/1" /root/user-openrc
 rm projects1a.txt
 rm projects1b.txt
 
 
 
 
-parse1d.py
-
-filename = 'projects1a.txt'
-arr=[]
-with open(filename) as fh:
-    for line in fh:
-        line = "".join(line.split())
-        command, description = line.strip().split("|", 1)
-        desc=command.strip()
-        if desc=="ID":
-                continue
-        arr.append(desc)
-print(arr)
-
-
-
-
-
-parse2d.py
-
-filename = 'projects1a.txt'
-arr=[]
-with open(filename) as fh:
-    for line in fh:
-        line = "".join(line.split())
-        command, description = line.strip().split("|", 1)
-        desc=description.strip()
-        if desc=="Name":
-                continue
-        arr.append(desc)
-print(arr)
-
-
-
-
-
-
-parse3d.py
-
-filename = 'projects1b.txt'
-arr=[]
-with open(filename) as fh:
-    for line in fh:
-        line = "".join(line.split())
-        a,b,c,d,e,f = line.strip().split("|", 5)
-        desc=a.strip()
-        if desc=="ID":
-                continue
-        arr.append(desc)
-print(arr)
-
-
-
-
-
-
-parse4d.py
-
-filename = 'projects1b.txt'
-arr=[]
-with open(filename) as fh:
-    for line in fh:
-        line = "".join(line.split())
-        a,b,c,d,e,f = line.strip().split("|", 5)
-        desc=b.strip()
-        if desc=="Name":
-                continue
-        arr.append(desc)
-print(arr)
-
-
-
-
-
-
-parse5d.py
-
-import json
-with open('test1.json') as input_file:
-    data = json.load(input_file)
-    with open('diagnostic_logs.json', 'a+') as out_file:
-        for e in data:
-           json.dump(e, out_file)
-           out_file.write('\n')
